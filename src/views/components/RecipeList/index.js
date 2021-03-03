c54af595a4ef29b2825adc69c1abc0d9d9d07601
Zahidul Hossain <choyan.009@gmail.com>
2021-03-03 11:22:13 +0600

    Recipe loading indicator

--- src/views/components/RecipeList/index.js
+++ src/views/components/RecipeList/index.js
@@ -1,14 +1,17 @@
 import { RecipeSingle } from '../index';
+import { Loading } from 'shared';
 
 export default function RecipeList({ loading, recipes }) {
   return (
     <div className="px-4 py-4">
       <h3 className="text-xl mb-4 leading-6 font-medium text-gray-900">Available Recipes.</h3>
-      {loading
-        ? 'loading'
-        : recipes.map((recipe) => {
-            return <RecipeSingle recipe={recipe} />;
-          })}
+      {loading ? (
+        <Loading />
+      ) : (
+        recipes.map((recipe) => {
+          return <RecipeSingle recipe={recipe} />;
+        })
+      )}
     </div>
   );
 }
